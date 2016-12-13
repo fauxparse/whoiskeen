e6785044aaec658876cb03491de7f6c783420897
Matt Powell <fauxparse@gmail.com>
2016-12-14 10:49:05 +1300

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Whoiskeen
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
