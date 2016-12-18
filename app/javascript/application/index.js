95f164ac90584cc4795b72bda34e441c159bc740
Matt Powell <fauxparse@gmail.com>
2016-12-19 12:25:13 +1300

    start routing

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import Clearance from './components/clearance'
+import reducer from './reducers'
 import Application from './components/application'
+
+const store = createStore(reducer, applyMiddleware(thunk))
+const history = syncHistoryWithStore(browserHistory, store)
 
 document.addEventListener("DOMContentLoaded", e => {
   ReactDOM.render(
-    <Application name="React" />,
+    <Provider store={store}>
+      <Clearance>
+        <Router history={history}>
+          <Route path="*" component={Application}/>
+        </Router>
+      </Clearance>
+    </Provider>,
     document.body.appendChild(document.createElement('div'))
   )
 })
